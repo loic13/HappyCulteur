42dddd49d8c4e046c9fecd5c8758edcf93b09dbe
Rémi Santiago <santiago.remi@gmail.com>
2017-01-07 20:41:14 +0100

    first commit

---
+++ wp-content/themes/llorix-one-lite/inc/admin/welcome-screen/sections/github.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Github
+ */
+?>
+
+<div id="github" class="llorix-one-lite-tab-pane">
+
+	<h1><?php esc_html_e( 'Can I Contribute?', 'llorix-one-lite' ); ?></h1>
+		
+	<p><?php esc_html_e( 'Found a bug?','llorix-one-lite'); ?></p>
+	<p><?php esc_html_e( 'Want to contribute with a fix or create a new feature?', 'llorix-one-lite'); ?></p>
+	<p><?php esc_html_e( 'GitHub is the place to go!','llorix-one-lite' ); ?></p>
+
+	<p>
+		<a href="https://github.com/Codeinwp/llorix-one-lite/" class="button button-primary"><?php esc_html_e( 'Llorix One Lite on GitHub', 'llorix-one-lite' ); ?></a>
+	</p>
+	
+	<p><?php esc_html_e( 'Or would you like to translate Llorix One Lite to your language? Get involved at wordpress.org.', 'llorix-one-lite' ); ?></p>
+	
+	<p>
+		<a href="<?php echo esc_url( 'https://translate.wordpress.org/projects/wp-themes/llorix-one-lite/' ); ?>" class="button button-primary"><?php _e( 'Translate Llorix One Lite', 'llorix-one-lite' ); ?></a>
+	</p>
+
+	<h4><?php esc_html_e( 'Are you enjoying Llorix One Lite?', 'llorix-one-lite' ); ?></h4>
+		
+	<p><?php echo sprintf( esc_html__( 'Leave us a review on %sWordPress.org%s. We\'d really appreciate it!', 'llorix-one-lite' ), '<a href="https://wordpress.org/support/theme/llorix-one-lite/">', '</a>' ); ?></p>
+	
+</div>
